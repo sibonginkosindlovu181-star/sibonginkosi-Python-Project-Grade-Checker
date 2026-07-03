7717e17053a27394e36036cabc71a87fae436370
sibonginkosindlovu181-star <sibonginkosindlovu181@gmail.com>
2026-07-03 05:43:22 -0700

    Add files via upload

---
+++ grade checker.py
@@ -0,0 +1,15 @@
+marks = input("enter your marks")
+marks = int(marks)
+
+if marks >= 80:
+    print("grade A")
+                   
+elif marks >= 70:
+     print("grade B")
+
+elif marks >= 50:
+    print("grade C")
+
+else:
+    print("fail")   
+
